5da8bc99ed36763e1e0cb0a831dbe6b119bacd74
tejaswaroopchitluri-beep <tejaswaroopchitluri@gmail.com>
2026-09-21 12:56:06 +0530

    fourth commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,21 +1,34 @@
 let express=require('express');
 let router=express.Router();
+let bycrypt=require('bcrypt');
 let {users}=require('../models/users');
 
 router.post("/register",async (req,res)=>{
     console.log(req.body);
+    req.body.password=await bycrypt.hash(req.body.password,10);
     let newuser=new users(req.body);
     let result=await newuser.save();
     res.send(result);
     
 })
-router.post("/login",(req,res)=>{
-    res.send('login page called');
+router.post("/login",async (req,res)=>{
+    let result=await users.findOne({email:req.body.email});
+    if(result){
+        let matchpass=await bycrypt.compare(req.body.password,result.password);
+        if(matchpass){
+            res.send("login successful");
+        }else{
+            res.send("login failed");
+        }       
+    }else{
+        res.send({result:"No user found"})
+    }
+
 })
-router.get("/viewtask",(req,res)=>{
+router.get("/viewtask",async (req,res)=>{
     res.send('view task page called');
 })
-router.put("/updatestatus",(req,res)=>{
+router.put("/updatestatus",async (req,res)=>{
     res.send('update status page called');
 })
 module.exports=router;
--- routes/hr_route.js
+++ routes/hr_route.js
@@ -1,11 +1,13 @@
 let express=require('express');
 let router=express.Router();
-
-router.get("/viewempolyees",(req,res)=>{
-    res.send('view empolyees route called');
-})
-router.post("/assign-task",(req,res)=>{
-    res.send('assign task route called');
+let {users}=require('../models/users');
+router.get("/viewempolyees",async (req,res)=>{
+   let result=await users.find();
+   res.send(result);
+});
+router.post("/assign-task",async (req,res)=>{
+    let result=await users.findByIdAndUpdate(req.body.id,{$set:{task:req.body.task}});
+    res.send(result);
 })
 router.get("/view-tasks",(req,res)=>{
     res.send('view tasks route called');
